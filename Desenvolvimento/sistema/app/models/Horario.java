c54ac609d9d7627ec8515509136eedef0a9b6880
Alexandre Mendes <alexandregme@gmail.com>
2013-10-01 17:21:38 -0300

    Ajustes nas views em geral

--- Desenvolvimento/sistema/app/models/Horario.java
+++ Desenvolvimento/sistema/app/models/Horario.java
@@ -12,6 +12,9 @@ import javax.persistence.ManyToOne;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Type;
+import org.joda.time.LocalDateTime;
+
 import play.db.jpa.GenericModel;
 
 /**
@@ -32,6 +35,16 @@ public class Horario extends GenericModel {
 	@JoinColumn(name = "id_turno")
 	public Turno turno;
 
+	
+//	@Column(name="horario_de", nullable = false)
+//	@Type(type="org.jadira.usertype.dateandtime.joda.PersistentLocalDateTime")
+//	public LocalDateTime horarioDe;
+//	
+//	@Column(name="horario_ate", nullable = false)
+//	@Type(type="org.jadira.usertype.dateandtime.joda.PersistentLocalDateTime")
+//	public LocalDateTime horarioAte;
+	//
+	
 	@Column(name = "horario_de")
 	public Date horarioDe;
 
